7e5b4c2a3fd21ca0ad57bda73b58f16e64e45b61
Vivek Gupta (DYNAMICS) <vigupta@microsoft.com>
2019-05-30 22:49:36 -0700

    Afinn,bing and nrc lexs

---
+++ SentimentAnalysis/1.Lexicons.R
@@ -0,0 +1,30 @@
+#####
+##' Sentiment analysis 
+##' tidy text format
+##' AFINN, bing et al. and nrc  Lexicons
+##' positive and negative sentimets. 
+##
+
+####Get the packages we need
+
+##install.packages("tibble")
+##install.packages("dplyr")
+##install.packages("tidytext")
+library(tidytext)
+library(tibble)
+library(dplyr)
+
+#this tibble frame is available from tidytext
+sentiments
+
+## afinn from -5 to +5 .. str
+get_sentiments("afinn")
+
+## bing binary 
+get_sentiments("bing")
+
+
+## nrc much much more .. 
+
+get_sentiments("nrc") %>% 
+  filter(sentiment == "negative" | sentiment == "positive")
