8a11822c37af16602eb177f16cf84ef2492776bb
Vivek Gupta (DYNAMICS) <vigupta@microsoft.com>
2019-05-31 01:00:07 -0700

    Comparing Jane  austen books

--- SentimentAnalysis/3.Extracting Sentiments using bing and in chunks.R
+++ SentimentAnalysis/3.Extracting Sentiments using bing and in chunks.R
@@ -12,12 +12,14 @@
 ##install.packages("tidytext")
 ##install.packages("stringr")
 ##install.packages("janeaustenr")
+##install.packages("ggplot2")
 library(tidytext)
 library(tibble)
 library(dplyr)
 library(stringr)
 library(janeaustenr)
 library(tidyr)
+library(ggplot2)
 
 ##  use austen books and convert them into tidy format 
 
@@ -37,12 +39,31 @@ tidy_emma_book <- tidy_books %>% filter(book == "Emma")
 
 bing_sent <- get_sentiments("bing")
 
- chunks_sentiment <- 
+
+## get the sentiments using Bing lex for Emma book and plot it 
+ chunks_sentiment_bing <- 
    tidy_emma_book %>% 
   inner_join(get_sentiments("bing")) %>% 
    mutate(chunk = linenumber %/% 80 ) %>%
    count(sentiment , chunk) %>% 
    spread(sentiment , n) %>% 
-  mutate(sentiment = positive - negative , ispos = ((positive - negative)>0 )) %>% 
-  count(chunk, ispos)
+  mutate(sentiment = positive - negative , ispos = ((positive - negative)>0 ))
+ #%>% 
+#  count(chunk, ispos)
+ ggplot(chunks_sentiment_bing , aes(chunk , sentiment)) + 
+   geom_col()
+ 
+ ## get the sentiments using Bing lex for All Jane Austen book and plot it 
+ 
+ jane_austen_chunks_sentiment <- tidy_books %>% 
+   inner_join(get_sentiments("bing")) %>% 
+   #mutate(chunk = linenumber %/% 80 ) %>%
+   count(book, chunk = linenumber %/% 80, sentiment ) %>% 
+   spread(sentiment , n) %>% 
+   mutate(sentiment = positive - negative , ispos = ((positive - negative)>0 ))
+ 
+ 
+ ggplot(jane_austen_chunks_sentiment , aes(chunk, sentiment, fill = book)) + 
+  geom_col() + 
+   facet_wrap(~book, ncol = 2)
  
